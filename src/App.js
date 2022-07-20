35ac3338304f52c6b880f924150b3321ebd702b7
Song4025 <thdgus4025@gmail.com>
2022-07-20 23:46:02 +0900

    리덕스 기초

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import "./App.css";
+import Box from "./component/Box";
 
 function App() {
+  let count = useSelector((state) => state.count);
+  let id = useSelector((state) => state.id);
+  let password = useSelector((state) => state.password);
+  // const [count, setCount] = useState(0);
+  const dispatch = useDispatch();
+  const login = () => {
+    dispatch({ type: "LOGIN", payload: { id: "song", password: "123" } });
+  };
+  const increaseCount = () => {
+    dispatch({ type: "INCREMENT", payload: { num: 5 } });
+    // setCount(count + 1);
+  };
+  const decreaseCount = () => {
+    dispatch({ type: "DECREMENT", payload: { num: 5 } });
+    // setCount(count - 1);
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>
+        {id},{password}
+      </h1>
+      <button onClick={login}>로그인</button>
+      <h1>{count}</h1>
+      <button onClick={increaseCount}>증가</button>
+      <Box />
+      <button onClick={decreaseCount}>감소</button>
     </div>
   );
 }
---
+++ src/redux/reducer/reducer.js
@@ -0,0 +1,23 @@
+let initialState = {
+  count: 0,
+  id: "",
+  password: "",
+};
+
+function reducer(state = initialState, action) {
+  console.log("action은?", action);
+  if (action.type === "INCREMENT") {
+    return { ...state, count: state.count + action.payload.num };
+  } else if (action.type === "DECREMENT") {
+    return { ...state, count: state.count - action.payload.num };
+  } else if (action.type === "LOGIN") {
+    return {
+      ...state,
+      id: action.payload.id,
+      password: action.payload.password,
+    };
+  }
+  return { ...state };
+}
+
+export default reducer;
